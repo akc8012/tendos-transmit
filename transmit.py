4e3cabb68f0b323b7e18eb55baca6450e7a2e6d7
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-15 15:22:20 -0400

    add button and labels

--- transmit.py
+++ transmit.py
@@ -1,7 +1,26 @@
 import tkinter as tk
 window = tk.Tk()
 
-label = tk.Label(text='TendosTransmit', font=('Cantarell', 'bold', 24))
-label.pack()
+
+def click_button():
+    print('you have touched me')
+
+
+font_face = 'Cantarell'
+
+tk.Label(
+    text='TendosTransmit',
+    font=(font_face, 24, 'bold')
+).pack()
+
+tk.Label(
+    text='The podcast deployment tool of the future.',
+    font=(font_face, 11, 'italic')
+).pack()
+
+tk.Button(
+    text='touch me',
+    command=click_button
+).pack()
 
 window.mainloop()
