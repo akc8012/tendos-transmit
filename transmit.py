57e26a224c38cfe3ae85081e8b8e248924f9bfe4
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-15 16:33:54 -0400

    pack label and description, set window title

--- transmit.py
+++ transmit.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 window = tk.Tk()
+window.title('TendosTransmit')
 
 
 def click_transmit():
@@ -18,14 +19,28 @@ tk.Label(
     font=(FONT_FACE, 11, 'italic')
 ).pack()
 
+tk.Label(
+    text='title'
+).pack(side='left')
+
 title = tk.StringVar()
 tk.Entry(
-    textvariable=title
-).pack()
+    textvariable=title,
+).pack(side='left')
+
+tk.Label(
+    text='description'
+).pack(side='left')
+
+# TODO: Use a grid to arrange these instead
+description = tk.StringVar()
+tk.Entry(
+    textvariable=description,
+).pack(side='left')
 
 tk.Button(
     text='transmit',
     command=click_transmit
-).pack()
+).pack(side='bottom')
 
 window.mainloop()
