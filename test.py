311f8bfd7576d9cc59451822815078919d03a4a9
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-20 10:37:39 -0400

    TestYamlWriter class, two tests for YamlWriter

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
 import unittest
+from transmit.YamlWriter import YamlWriter
+
+
+class TestYamlWriter(unittest.TestCase):
+    def test_yaml_write_single(self):
+        """
+        YamlWriter can output single key-value pair
+        """
+        data = {'hello': 'friends'}
+        result = YamlWriter().write(data)
+
+        self.assertEqual(result, 'hello: friends\n')
+
+    def test_yaml_write_multiple(self):
+        """
+        YamlWriter can output multiple key-value pairs
+        """
+        data = {
+            'goodbye': 'world',
+            'stonk_value': 1200
+        }
+        result = YamlWriter().write(data)
+
+        self.assertEqual(result, 'goodbye: world\nstonk_value: 1200\n')
