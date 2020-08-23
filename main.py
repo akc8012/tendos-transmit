a23cd0b40496b4a006e033d0ecf4df4f52a31345
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-23 11:34:22 -0400

    moved business logic to TransmitController, setup code is in main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import tkinter as tk
-from transmit.gui import TransmitGui
-from transmit.io import YamlFormatter
-from transmit.io import FileWriter
-
+from transmit.controller import TransmitController
 
 window = tk.Tk()
 window.title('TendosTransmit')
 
+TransmitController()
 
-def click_transmit(input):
-    data = YamlFormatter().format(input)
-    print(data)
-    FileWriter().write(data)
-
-
-transmit = TransmitGui(click_transmit)
 window.mainloop()
---
+++ transmit/controller.py
@@ -0,0 +1,13 @@
+from transmit.gui import TransmitGui
+from transmit.io import YamlFormatter
+from transmit.io import FileWriter
+
+
+class TransmitController:
+    def __init__(self):
+        gui = TransmitGui(self.click_transmit)
+
+    def click_transmit(self, input):
+        data = YamlFormatter().format(input)
+        print(data)
+        FileWriter().write(data)
