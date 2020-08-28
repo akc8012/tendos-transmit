dd87d38c8afb8ba00f16eec88b46f69fcf62f8e1
Andrew Colannino <akcolannino@email.neit.edu>
2020-08-28 16:17:31 -0400

    library to read wav, get_length method

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import tkinter as tk
+from scipy.io import wavfile
 from transmit.controller import TransmitController
+
+
+def get_length(rate, data):
+    return data.shape[0] / rate
+
+
+rate, data = wavfile.read('intro100.wav')
+
+length = get_length(rate, data)
+print(f"length = {length}s")
 
 window = tk.Tk()
 window.title('TendosTransmit')
