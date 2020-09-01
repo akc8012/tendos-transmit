c9822e5a2f4201026ec867cd5a6047e4b3bf375f
Andrew Colannino <akcolannino@email.neit.edu>
2020-09-01 10:43:54 -0400

    add current today to date field in yaml

--- transmit/controller.py
+++ transmit/controller.py
@@ -1,3 +1,5 @@
+import datetime
+
 from transmit.gui import TransmitGui
 from transmit.io import YamlFormatter
 from transmit.io import FileWriter
@@ -12,8 +14,10 @@ class TransmitController:
         if hasattr(self.gui, 'mp3_filename'):
             audio = AudioFile(self.gui.mp3_filename)
             # BUG: this should be in minutes, not seconds
+            # TODO: this should be in InputConversion class!!
             input['duration'] = audio.duration
             input['length'] = audio.file_size
+            input['date'] = datetime.date.today()
 
         data = YamlFormatter().format(input)
         print(data)
